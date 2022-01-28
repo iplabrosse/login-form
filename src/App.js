0dde44de2fd161ee196e5b3011f1bf8207cf0041
Isaiah LaBrosse <92277915+iplabrosse@users.noreply.github.com>
2022-01-28 15:33:12 -0500

    update files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.email.value);
+
+    if (!e.target.email.value) {
+      alert("Email is required");
+    } else if (!e.target.email.value) {
+      alert("Valid email is required");
+    } else if (!e.target.password.value) {
+      alert("Password is required");
+    } else if (
+      e.target.email.value === "me@example.com" &&
+      e.target.password.value === "123456"
+    ) {
+      alert("Successfully logged in");
+      e.target.email.value = "";
+      e.target.password.value = "";
+    } else {
+      alert("Wrong email or password combination");
+    }
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+
+    alert("Goes to registration page");
+  };
+  
+  render() {
+    return (
+      <div className="App">
+      <div class="topnav">
+          <a class="active" href="#home">Home</a>
+          <a href="#login">Login</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+        </div>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="name@email.com" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" />
+          </div>
+          <button className="primary">Log In</button>
+        </form>
+        <button className="secondary" onClick={this.handleClick}>
+          Create a New Account
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
